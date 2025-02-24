ca2afbdc2363720b1376d2d29d98f3cdc74efd1c
Sachin Kadam <sachin.kadam@calsoftinc.com>
2025-02-24 14:27:29 +0000

    Make changes to the bazel files

--- .devcontainer/install_bazel.sh
+++ .devcontainer/install_bazel.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-# Install prerequisites
-sudo apt update && sudo apt install -y apt-transport-https curl gnupg
+# Install bazelisk
+curl -LO https://github.com/bazelbuild/bazelisk/releases/latest/download/bazelisk-linux-amd64
+sudo mv bazelisk-linux-amd64 /usr/local/bin/bazel
+sudo chmod +x /usr/local/bin/bazel
 
-# Add Bazel's official APT repository and GPG key
-curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
-sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
-sudo apt update
-
-# Install Bazel (replace 5.0.0 with the latest version if needed)
-sudo apt install -y bazel-5.0.0
-
-# Install Bazel's latest version
-sudo apt install -y bazel
-sudo apt update && sudo apt full-upgrade -y
-
-# Check Bazel installation
+# Verify bazelisk installation
 bazel --version
